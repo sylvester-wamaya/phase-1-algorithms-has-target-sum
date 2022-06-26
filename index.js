936dbb4530d3149839abd88b0a8fb6a2866a2a94
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-26 22:25:10 +0300

    commit tree 4835123e587740cb569b948aa2d5004c67b7f876

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // iterate in all arrays 
+  //look for a differance between target and array
+  //check if the difference is same as the target.
+  for (let i = 0; i < array.length; i++) {
+    const diff = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if(array[j] === diff) return true;      
+    }
+    
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
-*/
-
+*/ 
+// (O)n2
 /* 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+  The code checks a difference between an array and the target
+  it then comapres the differnce with the rest if the array elements
+  it returns true if they're equal.
 */
 
 // You can run `node index.js` to view these console logs
